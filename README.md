e4c8eb0c15b8dadb270158f31b9d2fd5d8a658d2
Amaan Hashmi-Ubhi <AmaanHUB@gmail.com>
2020-11-24 15:13:28 +0000

    Added all stuff to provision and finished readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Vagrant Provisioning Task With A JS App
+
+## Requirements
+* Vagrant
+* VirtualBox
+* Git
+
+* Make sure these are installed before doing any of this
+* These can be installed from your relevant package manager
+
+
+## Get What You Need
+
+* Clone the git directory with `git clone`
+* Check the presence of all the files
+* Run:
+```
+vagrant up
+```
+
+* **Congratulations, everything should be working now**
+
+
+## Check
+
+* Check if the webpage exists
+* In your browser, type in `testing.local:3000`
+
--- environment/provision.sh
+++ environment/provision.sh
@@ -17,5 +17,29 @@ sudo apt install nodejs -y
 # Install npm (needs to do separately)
 sudo apt install npm -y
 
+# Install MongoDb
+sudo apt install mongodb -y
+
 # Install pm2 with npm
 sudo npm install pm2
+
+# Restart nginx just in case
+sudo systemctl restart nginx.service
+
+# Restart mongodb just in case
+sudo systemctl restart mongodb.service
+
+# Move to /app repository
+cd /app
+
+# Install mongoose (requirements for app)
+sudo npm install mongoose
+
+# Install faker (requirements for app)
+sudo npm install faker
+
+# Install the app dependencies
+sudo npm install
+
+# Run the app
+sudo npm start &
